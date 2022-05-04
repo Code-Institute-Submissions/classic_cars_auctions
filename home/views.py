0a2ef0fcf095636e1fcd2d9a6021abd3fe2eedb7
Jakub Essalhi <usksidi@gmail.com>
2022-05-04 04:11:43 +0000

    displayed last sold item on the home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from auctions.models import Bid, Car
 
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    highest_bid = Bid.objects.filter(winnerBid=True).last().amount
+    highest_bid_id = Bid.objects.filter(winnerBid=True).last().car.id
+    print(highest_bid_id)
+ 
+    if highest_bid:
+        car = get_object_or_404(Car, id=highest_bid_id)
+
+    context = {
+        'car': car,
+        'highest_bid': highest_bid,
+    }
+    return render(request, 'home/index.html', context)
