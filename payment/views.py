84c2a0c44991cc9a144c167a01f452e587597bda
Jakub Essalhi <usksidi@gmail.com>
2022-05-07 18:11:42 +0000

    added basic payment functionality

--- payment/views.py
+++ payment/views.py
@@ -1,29 +1,44 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from auctions.models import Bid
+from django.conf import settings
 
+import stripe
+
+from auctions.models import Bid
 from .forms import PaymentForm
 
 
 @login_required()
 def payment(request, highest_bid_obj_id):
     """View to render Payment"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     deposit = 0
     payment_form = PaymentForm()
     winner_bid = Bid.objects.filter(id=highest_bid_obj_id)
-    if winner_bid[0].winnerBid and request.user.id == winner_bid[0].user_id:
-        deposit = winner_bid[0].amount / 10
-    else:
+    if not (winner_bid[0].winnerBid and 
+           (request.user.id == winner_bid[0].user_id)):
         messages.error(request, 'Unauthorized Access.')
         return redirect(reverse('all_auctions'))
 
+    deposit = winner_bid[0].amount / 10
+    stripe_total = round(deposit * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
     context = {
        'winner_bid': winner_bid,
        'payment_form': payment_form,
        'deposit': deposit,
-       'stripe_public_key': 'pk_test_51KgHIsA2xS5xmfsxSeIRUuMEGtdvUMiDOu6i20EFqkp5s9gcR0WGDQMhWwrbAqMa3oV8hF52GTD5HsRqyVUY7dt200R7AllYsj',
-       'client_secret': 'test client secret',
+       'stripe_public_key': stripe_public_key,
+       'client_secret': intent.client_secret,
     }
 
     return render(request, 'payment/payment.html', context)
