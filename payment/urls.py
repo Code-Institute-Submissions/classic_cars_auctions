01a1c966890976abaf1cfda838a4eb40b561615d
Jakub Essalhi <usksidi@gmail.com>
2022-05-10 05:13:10 +0000

    added payment success URL

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payment, name='payment')
+    path('', views.get_payment, name='get_payment'),
+    path('payment_success/<payment_number>', views.payment_success, name='payment_success'),
 ]
