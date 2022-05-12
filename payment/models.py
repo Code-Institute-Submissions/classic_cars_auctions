fea3403062f0e39fad1ade58c73a73fa1675cb67
Jakub Essalhi <usksidi@gmail.com>
2022-05-12 05:27:01 +0000

    changed country field in payment model from CharField to countryField

--- payment/models.py
+++ payment/models.py
@@ -1,15 +1,17 @@
+from django_countries.fields import CountryField
+
 from django.db import models
 from auctions.models import Bid, Car
 
 
 class Payment(models.Model):
     """Payment Model"""
-    payment_number = models.CharField(max_length=32, null=False, 
+    payment_number = models.CharField(max_length=32, null=False,
                                       editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=50, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -17,6 +19,7 @@ class Payment(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     deposit = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    car_id = models.IntegerField()
 
     # def _generate_order_number(self):
     #     """
