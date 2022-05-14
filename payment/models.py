141cae7d94a0cc7fd3accda17c88fc10e476e962
Jakub Essalhi <usksidi@gmail.com>
2022-05-14 09:18:35 +0000

    changed payment model by removing paymentInlineItem Model and adding car and bids field to Payment Model

--- payment/models.py
+++ payment/models.py
@@ -11,6 +11,10 @@ class Payment(models.Model):
                                       editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='payment')
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL,
+                            null=True, blank=True, related_name='car')
+    bids = models.ForeignKey(Bid, on_delete=models.SET_NULL,
+                             null=True, blank=True, related_name='Bid')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -22,46 +26,4 @@ class Payment(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     deposit = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    car_id = models.IntegerField()
 
-    # def _generate_order_number(self):
-    #     """
-    #     Generate a random, unique order number using UUID
-    #     """
-    #     return uuid.uuid4().hex.upper()
-
-    # def save(self, *args, **kwargs):
-    #     """
-    #     Override the original save method to set the order number
-    #     if it hasn't been set already.
-    #     """
-    #     if not self.payment_number:
-    #         self.payment_number = self._generate_order_number()
-    #     super().save(*args, **kwargs)
-
-    def __str__(self):
-        return str(self.payment_number)
-
-
-class PaymentLineItem(models.Model):
-    """paymentLineItem Model"""
-    payment = models.ForeignKey(Payment, null=False, blank=False,
-                                on_delete=models.CASCADE,
-                                related_name='lineitems')
-    car = models.ForeignKey(Car, null=False, blank=False,
-                            on_delete=models.CASCADE)
-    winner_bid = models.ForeignKey(Bid, null=False,
-                                   blank=False, on_delete=models.CASCADE)
-    car_price = models.IntegerField()
-    
-
-    # def save(self, *args, **kwargs):
-    #     """
-    #     Override the original save method 
-    #     and update the deposit.
-    #     """
-    #     self.deposit = self.winner_bid.amount / 10
-    #     super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f'{self.car.make} on order {self.payment.payment_number}'
