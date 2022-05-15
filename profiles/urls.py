5bb545fe698e2da62d07e1294471c08cc5b87fa2
Jakub Essalhi <usksidi@gmail.com>
2022-05-15 01:54:01 +0000

    added payment and bidding history

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('payment_history/<payment_number>', views.payment_history, name='payment_history'),
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
+from payment.models import Payment
+from auctions.models import Bid
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
+    user_id = request.user.id
     profile = get_object_or_404(UserProfile, user=request.user)
+    user_bids = Bid.objects.filter(user_id=user_id)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -20,8 +25,28 @@ def profile(request):
 
     template = 'profiles/profile.html'
     context = {
-        # 'profile': profile,
+        'payments': payments,
         'form': form,
+        'user_bids':  user_bids,
+        'on_profile_page': True,
     }
+    return render(request, template, context)
+
+
+def payment_history(request, payment_number):
+    """views  to render payment history"""
+    payment = get_object_or_404(Payment, payment_number=payment_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for a payment number {payment_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'payment/payment_success.html'
+    context = {
+        'payment': payment,
+        'from_profile': True,
+    }
+
     return render(request, template, context)
 
