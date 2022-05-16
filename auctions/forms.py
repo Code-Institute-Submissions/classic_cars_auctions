0d59550e0257caf457b508927b09abfb40099d60
Jakub Essalhi <usksidi@gmail.com>
2022-05-16 01:50:02 +0000

    created CarForm to add a car to auctions

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Car
 
 
 class BidForm(forms.Form):
@@ -9,3 +10,17 @@ class BidForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.fields['user_bid'].required = False
         self.label_suffix = ""
+
+class CarForm(forms.ModelForm):
+    """Car form"""
+    class Meta:
+        """CarForm Meta"""
+        model = Car
+        fields = '__all__'
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'border-black rounded-0 form-control'
+
