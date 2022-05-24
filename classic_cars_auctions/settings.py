ba08016dec67b20e6e9efe874e931f05aa82f012
Jakub Essalhi <usksidi@gmail.com>
2022-05-24 16:16:12 +0000

    removed secret key and set debug

--- classic_cars_auctions/settings.py
+++ classic_cars_auctions/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^xrem&bvj*$i2&v2^xmr+_f1a#d+sw63oycmcrdxnm+sr0^o0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['classic-cars-auctions.herokuapp.com', 'localhost']
 
