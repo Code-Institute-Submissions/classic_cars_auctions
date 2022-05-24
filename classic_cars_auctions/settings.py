2fea4e62291b44e246cbff26bfa8b0fbdd3703c1
Jakub Essalhi <usksidi@gmail.com>
2022-05-24 13:09:37 +0000

    deployment part 2

--- classic_cars_auctions/settings.py
+++ classic_cars_auctions/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g^xrem&bvj*$i2&v2^xmr+_f1a#d+sw63oycmcrdxnm+sr0^o0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['classic-cars-auctions.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     "bootstrap4",
     "bootstrap_datepicker_plus",
-    # "apscheduler"
+    
 ]
 
 MIDDLEWARE = [
@@ -115,20 +115,23 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
