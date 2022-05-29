a58bd90bbbfa394662e07c5473cb3a28de591b76
Jakub Essalhi <usksidi@gmail.com>
2022-05-29 17:26:36 +0000

    changed updater interval to 15 minutes

--- auctions/updater.py
+++ auctions/updater.py
@@ -5,6 +5,6 @@ from .views import get_winner_bid, check_payments
 def start():
     """function to run scheduler"""
     scheduler = BackgroundScheduler()
-    scheduler.add_job(check_payments, 'interval', minutes=30)
-    scheduler.add_job(get_winner_bid, 'interval', minutes=30)
+    scheduler.add_job(check_payments, 'interval', minutes=15)
+    scheduler.add_job(get_winner_bid, 'interval', minutes=15)
     scheduler.start()
