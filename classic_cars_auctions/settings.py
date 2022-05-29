6a86d347a84a6ab98e81be3db5b56c0f01b73989
Jakub Essalhi <usksidi@gmail.com>
2022-05-29 14:53:51 +0000

    fixed HTML error in auction_detail template

--- classic_cars_auctions/settings.py
+++ classic_cars_auctions/settings.py
@@ -208,10 +208,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-# if 'DEVELOPMENT' in os.environ:
-#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#     DEFAULT_FROM_EMAIL = 'classiccarsauctions@example.com'
-# else:
+# email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
